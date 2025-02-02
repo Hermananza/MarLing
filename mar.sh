dec7f260410c5d556c67cf4522a64aa27617fd52
Hermananza <101302614+Hermananza@users.noreply.github.com>
2025-02-03 04:34:27 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -254,7 +254,7 @@ echo "<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EDtunnel-rev - VLESS Proxy</title>
+    <title>HC STORE - Sedia Akun Vpn</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
